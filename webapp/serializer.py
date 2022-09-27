ba3cc2a93299a2e21dfc8009b56fbe7e5875b456
Murat Alaichiev <murat.alaichiev@gmail.com>
2022-09-27 18:07:29 +0600

    created serializer.py with 2 options:
    basic serializer
    serializer to put method

--- webapp/serializer.py
+++ webapp/serializer.py
@@ -1,8 +1,22 @@
 from rest_framework import serializers
-from rest_framework.authtoken.admin import User
+from webapp.models import Quote
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class QuoteUpdateSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User
-        fields = ['url', 'username', 'email', 'is_staff']
+        model = Quote
+        fields = '__all__'
+        read_only_fields = ['author', 'rating', 'created_at', 'email']
+
+
+class QuoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Quote
+        fields = '__all__'
+        read_only_fields = ['rating', 'status', 'created_at']
+
+    def validate(self, attrs):
+        return super().validate(attrs)
+
+    def create(self, validated_data):
+        return Quote.objects.create(**validated_data)
